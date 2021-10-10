ab573dd05700ca4d0e8494dc8772b90541acf047
Dimitri Harding <vaughnharding@hotmail.com>
2021-10-10 16:45:10 -0500

    feat: generate final art

--- art_engine/src/config.js
+++ art_engine/src/config.js
@@ -5,33 +5,61 @@ const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
-  "The Supacoin project was created during the 2021 Hacktoberfest Supabase hackathon. \n\n This is a collection of 2020 uniquely generated, colorful, and swaggable NFTs living on the Polygon Chain.  \n\n\n This was made with Supabase lovers in mind, and tools that are used with Supabase.";
+  "The Supacoin project was created during the 2021 Hacktoberfest Supabase hackathon. \n\n This is a collection of 800 uniquely generated, colorful, and swaggable NFTs living on the Polygon Chain.  \n\n\n It was made with Supabase lovers in mind, and tools that are used with Supabase.";
 const baseUri =
-  "https://guywxbvrqiimbwosujbe.supabase.in/storage/v1/object/public/supacoin-store/images";
+  "https://guywxbvrqiimbwosujbe.supabase.in/storage/v1/object/public/supacoin-prod/images";
 
 const layerConfigurations = [
   {
     growEditionSizeTo: 100,
-    layersOrder: [
-      { name: "Pattern" },
-      { name: "Supacoin" },
-      { name: "Watermark" },
-    ],
+    layersOrder: [{ name: "Pattern" }, { name: "Supacoin" }],
   },
   {
     growEditionSizeTo: 200,
-    layersOrder: [
-      { name: "Plain" },
-      { name: "Supacoin" },
-      { name: "Watermark" },
-    ],
+    layersOrder: [{ name: "Plain" }, { name: "Supacoin" }],
   },
   {
     growEditionSizeTo: 300,
+    layersOrder: [{ name: "Gradient" }, { name: "Supacoin" }],
+  },
+  {
+    growEditionSizeTo: 350,
+    layersOrder: [{ name: "Pattern" }, { name: "Supacoin" }, { name: "Emoji" }],
+  },
+  {
+    growEditionSizeTo: 400,
     layersOrder: [
-      { name: "Gradient" },
+      { name: "Plain" },
       { name: "Supacoin" },
-      { name: "Watermark" },
+      { name: "IF YOU KNOW" },
+    ],
+  },
+  {
+    growEditionSizeTo: 500,
+    layersOrder: [{ name: "Pattern" }, { name: "Supacoin Posterize" }],
+  },
+  {
+    growEditionSizeTo: 600,
+    layersOrder: [{ name: "Plain" }, { name: "Supacoin Posterize" }],
+  },
+  {
+    growEditionSizeTo: 700,
+    layersOrder: [{ name: "Gradient" }, { name: "Supacoin Posterize" }],
+  },
+  {
+    growEditionSizeTo: 750,
+    layersOrder: [
+      { name: "Pattern" },
+      { name: "Supacoin Posterize" },
+      { name: "Emoji" },
+    ],
+  },
+  {
+    growEditionSizeTo: 800,
+    layersOrder: [
+      { name: "Plain" },
+      { name: "Supacoin Posterize" },
+      { name: "IF YOU KNOW" },
     ],
   },
 ];
