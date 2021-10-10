4619d9901850cac07eac7bb6491ead28c8e5702c
Dimitri Harding <vaughnharding@hotmail.com>
2021-10-09 23:33:19 -0500

    refactor: update web3Context with more functions for interacting with contract

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,112 +1,126 @@
 import React from "react";
-import { Stack, Button, useBreakpointValue, Link } from "@chakra-ui/react";
+import {
+  Stack,
+  Button,
+  useBreakpointValue,
+  Link,
+  Text,
+  Box,
+} from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { SEO } from "../components/SEO";
 import { Hero } from "../components/HomepageSections/Hero";
 import { GettingStarted } from "../components/HomepageSections/GettingStarted";
 import { CTASection } from "../components/HomepageSections/CTASection";
 import { DiscordBanner } from "../components/HomepageSections/DiscordBanner";
-import {
-  checkIfWalletIsConnectedAndGetAccount,
-  connectWallet,
-  mintNFT,
-} from "../utils/web3";
 import { useNotification } from "../utils/feedback";
 import ConnectButton from "../components/ConnectButton";
 import MintButton from "../components/MintButton";
 import { useWeb3 } from "../utils/web3Context";
 
 const Home = () => {
-  const { setAccount, account } = useWeb3();
+  const {
+    account,
+    checkIfWalletIsConnected,
+    connectWallet,
+    mintNFT,
+    mintedTokenLink,
+  } = useWeb3();
   const { notify } = useNotification();
   const [loading, setLoading] = React.useState(false);
   const isMobile = useBreakpointValue({ base: true, md: false });
-  React.useEffect(async () => {
-    const { data, error } = await checkIfWalletIsConnectedAndGetAccount();
-    if (data) {
-      setAccount(data.account);
-    }
 
-    if (error) {
-      console.log(error);
-    }
+  React.useEffect(async () => {
+    await checkIfWalletIsConnected();
   }, []);
 
   return (
     <>
       <SEO />
-      <Hero animate={loading}>
-        <Stack
-          direction={{ base: "column", sm: "row" }}
-          mb={{ base: 2, md: 4 }}
-          spacing={2}
-          justifyContent={{ sm: "left", md: "center" }}
-        >
-          {!account ? (
-            <ConnectButton
-              onClick={async () => {
-                const { data, error } = await connectWallet();
-                if (data) {
-                  setAccount(data.account);
-                }
+      <Hero animate={loading} mintedTokenLink={mintedTokenLink}>
+        <div>
+          <Stack
+            direction={{ base: "column", sm: "row" }}
+            mb={{ base: 2, md: 4 }}
+            spacing={2}
+            justifyContent={{ sm: "left", md: "center" }}
+          >
+            {!account ? (
+              <ConnectButton
+                onClick={async () => {
+                  const { error } = await connectWallet();
 
-                if (error) {
-                  console.log(error);
-                  notify({
-                    title: "Get a Metamask Wallet",
-                    description: error.message,
-                    status: "error",
-                  });
-                }
-              }}
-            />
-          ) : (
-            <MintButton
-              isLoading={loading === true}
-              onClick={() => {
-                setLoading(true);
-                mintNFT()
-                  .then(async (response) => {
-                    await response.wait();
+                  if (error) {
                     notify({
-                      title: "Your Supacoin was successfully minted!",
-                      description: "",
-                      status: "success",
-                    });
-                    setLoading(false);
-                  })
-                  .catch((error) => {
-                    console.log({ error });
-                    notify({
-                      title: "Could not mint your supacoin!",
+                      title: "Get a Metamask Wallet",
                       description: error.message,
                       status: "error",
                     });
-                    setLoading(false);
-                  });
-              }}
-            />
-          )}
-          {process.env.NEXT_PUBLIC_OPENSEA_LINK && (
-            <Button
-              fontWeight="bold"
-              borderRadius="md"
-              size="lg"
-              colorScheme="teal"
-              variant="ghost"
-              isActive={isMobile}
-            >
-              <Link
-                alignItems="center"
-                href={process.env.NEXT_PUBLIC_OPENSEA_LINK}
-                isExternal
+                  }
+                }}
+              />
+            ) : (
+              <MintButton
+                isLoading={loading === true}
+                onClick={() => {
+                  setLoading(true);
+                  mintNFT()
+                    .then(async (transaction) => {
+                      console.log({ transaction });
+                      await transaction.wait();
+                      notify({
+                        title: "Your Supacoin was successfully minted!",
+                        description: (
+                          <Text>
+                            "You can now view your freshly minted Supacoin on
+                            Opensea. Sometimes it takes a few minutes to show
+                            up.
+                            <Link
+                              alignItems="center"
+                              href={mintedTokenLink}
+                              isExternal
+                            >
+                              {mintedTokenLink}
+                              <ExternalLinkIcon mx="2px" />
+                            </Link>
+                          </Text>
+                        ),
+                        status: "success",
+                      });
+                      setLoading(false);
+                    })
+                    .catch((error) => {
+                      notify({
+                        title: "Could not mint your Supacoin!",
+                        description: "Please try again later.",
+                        status: "error",
+                      });
+                      setLoading(false);
+                    });
+                }}
+              />
+            )}
+            {process.env.NEXT_PUBLIC_OPENSEA_LINK && (
+              <Button
+                fontWeight="bold"
+                borderRadius="md"
+                size="lg"
+                colorScheme="teal"
+                variant="ghost"
+                isActive={isMobile}
               >
-                View collection on Opensea{" "}
-                {!isMobile && <ExternalLinkIcon mx="2px" />}
-              </Link>
-            </Button>
-          )}
-        </Stack>
+                <Link
+                  alignItems="center"
+                  href={process.env.NEXT_PUBLIC_OPENSEA_LINK}
+                  isExternal
+                >
+                  View collection on Opensea{" "}
+                  {!isMobile && <ExternalLinkIcon mx="2px" />}
+                </Link>
+              </Button>
+            )}
+          </Stack>
+        </div>
       </Hero>
       <GettingStarted />
       <CTASection />
--- app/utils/web3Context.js
+++ app/utils/web3Context.js
@@ -1,7 +1,26 @@
 import React, { useState, useEffect, useContext, createContext } from "react";
 import { ethers } from "ethers";
-import { checkIfWalletIsConnectedAndGetAccount } from "../utils/web3";
+import SupacoinNFT from "./SupacoinNFT.json";
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+
+const getConnectedContract = () => {
+  const { ethereum } = window;
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const connectedContract = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    SupacoinNFT.abi,
+    signer
+  );
+
+  return connectedContract;
+};
+
 const web3Context = createContext();
+const OPEN_SEA_URL =
+  process.env.NEXT_PUBLIC_APP_MODE === "test"
+    ? "https://testnets.opensea.io"
+    : "https://opensea.io";
 
 const networksMap = {
   1: {
@@ -47,6 +66,48 @@ export const useWeb3 = () => {
 function useWeb3Provider() {
   const [account, setAccount] = useState(null);
   const [network, setNetwork] = useState(null);
+  const [mintedTokenLink, setMintedTokenLink] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      return {
+        data: null,
+        error: {
+          message: "Make sure you have metamask!",
+        },
+      };
+    } else {
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      /*
+       * User can have multiple authorized accounts, we grab the first one if its there!
+       */
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        // Setup listener! This is for the case where a user comes to our site
+        // and ALREADY had their wallet connected + authorized.
+        setupEventListener();
+        setAccount(account);
+        return {
+          error: null,
+          data: {
+            account,
+          },
+        };
+      } else {
+        console.log("No authorized account found");
+        setAccount(null);
+        return {
+          data: null,
+          error: null,
+        };
+      }
+    }
+  };
 
   const switchAccount = async () => {
     await window.ethereum.request({
@@ -57,14 +118,84 @@ function useWeb3Provider() {
         },
       ],
     });
-    const { data, error } = await checkIfWalletIsConnectedAndGetAccount();
-    if (data) {
-      setAccount(data.account);
-    }
+    await checkIfWalletIsConnected();
+  };
 
-    if (error) {
+  /*
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    const { ethereum } = window;
+    try {
+      if (!ethereum) {
+        return {
+          error: {
+            message: "You need a crypto wallet to interact with this website.",
+          },
+          data: null,
+        };
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      const account = accounts[0];
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+      setAccount(account);
+      return {
+        error: null,
+        data: {
+          account,
+        },
+      };
+    } catch (error) {
       setAccount(null);
+      console.log(error);
+      return {
+        error,
+        data: null,
+      };
     }
+  };
+
+  const setupEventListener = async () => {
+    const { ethereum } = window;
+    try {
+      if (ethereum) {
+        const connectedContract = getConnectedContract();
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewSupacoinNFTMinted", async (from, tokenId) => {
+          const [account] = await ethereum.request({ method: "eth_accounts" });
+          console.log({ account });
+          console.log({ from });
+          setMintedTokenLink(
+            `${OPEN_SEA_URL}/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const mintNFT = () => {
+    const supacoinContract = getConnectedContract();
+    return supacoinContract.mint();
   };
 
   useEffect(() => {
@@ -79,6 +210,11 @@ function useWeb3Provider() {
           setNetwork(detectedNetwork);
         }
       });
+
+      ethereum.on("accountsChanged", (accounts) => {
+        const [account] = accounts;
+        setAccount(account);
+      });
     }
   }, []);
 
@@ -87,5 +223,9 @@ function useWeb3Provider() {
     network,
     setAccount,
     switchAccount,
+    mintedTokenLink,
+    connectWallet,
+    checkIfWalletIsConnected,
+    mintNFT,
   };
 }
